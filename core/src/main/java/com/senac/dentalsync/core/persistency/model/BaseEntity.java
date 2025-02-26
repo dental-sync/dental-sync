cd54f50c69115778c02e8b0622736b8e8d1d2b8e
VICTOR LEANDRO VIEIRA MORA <victor.mora1@alunos.sc.senac.br>
2025-02-26 20:05:16 -0300

    feat: adiciona backend ao projeto.

---
+++ core/src/main/java/com/senac/dentalsync/core/persistency/model/BaseEntity.java
@@ -0,0 +1,37 @@
+package com.senac.dentalsync.core.persistency.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "is_active")
+    private Boolean isActive;
+
+    @Column(name = "created_by")
+    private Usuario createdBy;
+
+    @Column(name = "updated_by")
+    private Usuario updatedBy;
+
+}
---
+++ core/src/main/java/com/senac/dentalsync/core/persistency/model/Usuario.java
@@ -0,0 +1,31 @@
+package com.senac.dentalsync.core.persistency.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private String role;
+
+}

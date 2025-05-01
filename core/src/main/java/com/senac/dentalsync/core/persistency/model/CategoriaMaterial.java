58c52ba624d62c7ace7d500abe613eee677000cc
eqMoura01 <victorlvmora@gmail.com>
2025-05-01 11:48:33 -0300

    fix: adiciona limitacao de caracteres ao nome da categoria.

--- core/src/main/java/com/senac/dentalsync/core/persistency/model/CategoriaMaterial.java
+++ core/src/main/java/com/senac/dentalsync/core/persistency/model/CategoriaMaterial.java
@@ -2,6 +2,7 @@ package com.senac.dentalsync.core.persistency.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,5 +14,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "categoria_material")
 public class CategoriaMaterial extends BaseEntity{
     
+    // Adiciona limitacao de caracteres
+    @Size(max = 40, message = "O nome da categoria deve ter no máximo 40 caracteres")
     private String nome; 
 }

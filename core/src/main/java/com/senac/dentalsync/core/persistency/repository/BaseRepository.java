09129d0740389ac68dc85bcfb1dd1ecdb943c060
VICTOR LEANDRO VIEIRA MORA <victor.mora1@alunos.sc.senac.br>
2025-05-07 19:50:27 -0300

    feat: adiciona logica de material

--- core/src/main/java/com/senac/dentalsync/core/persistency/repository/BaseRepository.java
+++ core/src/main/java/com/senac/dentalsync/core/persistency/repository/BaseRepository.java
@@ -1,8 +1,11 @@
 package com.senac.dentalsync.core.persistency.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
 @NoRepositoryBean
 public interface BaseRepository<T, ID> extends JpaRepository<T, ID> {
+    List<T> findAllByIsActiveTrue();
 } 
---
+++ core/src/main/java/com/senac/dentalsync/core/service/MaterialService.java
@@ -0,0 +1,39 @@
+package com.senac.dentalsync.core.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.senac.dentalsync.core.persistency.model.Material;
+import com.senac.dentalsync.core.persistency.model.Usuario;
+import com.senac.dentalsync.core.persistency.repository.MaterialRepository;  
+
+@Service
+public class MaterialService extends BaseService<Material, Long> {
+
+    @Autowired
+    private MaterialRepository materialRepository;
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @Override
+    public MaterialRepository getRepository() {
+        return materialRepository;
+    }
+
+    @Override
+    public Usuario getUsuarioLogado() {
+        return usuarioService.getUsuarioLogado();
+    }
+
+    public Material updateStatus(Long id, Boolean status) {
+        Material material = materialRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Material não encontrado"));
+        material.setIsActive(status);
+        return materialRepository.save(material);
+    }
+    
+    public void delete(Long id) {
+        materialRepository.deleteById(id);
+    }
+}

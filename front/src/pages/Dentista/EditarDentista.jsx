f4e598572445642b1e44005541db8cdd37df8ec1
Wesley Dutra <Wesley-Dutra17@hotmail.com>
2025-05-15 18:55:24 -0300

    feat: validação de novo

--- front/src/pages/Dentista/EditarDentista.jsx
+++ front/src/pages/Dentista/EditarDentista.jsx
@@ -63,8 +63,10 @@ const EditarDentista = () => {
       newErrors.nome = 'O nome é obrigatório';
     } else if (formData.nome.trim().split(' ').length < 2) {
       newErrors.nome = 'Por favor, informe o nome e sobrenome';
-    } else if (formData.nome.trim().split(' ').some(part => part.length < 2)) {
-      newErrors.nome = 'Sobrenome deve possuir no mínimo 2 letras';
+    } else if (formData.nome.trim().split(' ')[0].length < 2) {
+      newErrors.nome = 'O nome deve possuir no mínimo 2 letras';
+    } else if (formData.nome.trim().split(' ').pop().length < 2) {
+      newErrors.nome = 'O último sobrenome deve possuir no mínimo 2 letras';
     } else if (formData.nome.length > 255) {
       newErrors.nome = 'O nome não pode ultrapassar 255 caracteres';
     } else if (/\d/.test(formData.nome)) {

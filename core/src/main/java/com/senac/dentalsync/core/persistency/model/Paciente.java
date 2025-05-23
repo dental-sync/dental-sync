c8a18c773bc4a8018b30f5b4253f04a33c644dd2
Nathan Rodrigues <nathanping73@gmail.com>
2025-05-23 20:04:06 -0300

    fix: ajustado o campo nome, tinha sido deletado

--- core/src/main/java/com/senac/dentalsync/core/persistency/model/Paciente.java
+++ core/src/main/java/com/senac/dentalsync/core/persistency/model/Paciente.java
@@ -29,7 +29,7 @@ public class Paciente extends BaseEntity {
     message = "Por favor, informe nome e sobrenome válido"
 )
     @Size(max = 255, message = "O nome não pode ultrapassar 255 caracteres")
-
+    private String nome;
     @NotBlank(message = "O telefone é obrigatório")
     @Pattern(regexp = "\\(\\d{2}\\)\\s(\\d{4}-\\d{4}|\\d{5}-\\d{4})", message = "Formato de telefone inválido. Use o formato: (99) 9999-9999 ou (99) 99999-9999")
     private String telefone;

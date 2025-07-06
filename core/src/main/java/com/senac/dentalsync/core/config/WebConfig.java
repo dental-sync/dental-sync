b47f0529276d77a64756030516971a24ae939d14
CarusoAugusto <103348874+ragnarcb@users.noreply.github.com>
2025-07-06 03:00:15 -0300

    feat: adição de serviços frontend e certbot no docker-compose, atualização da configuração do app para MySQL, melhorias nas configurações de CORS e ajustes na configuração do n8n, incluindo variáveis de ambiente e acesso via IP público.

--- core/src/main/java/com/senac/dentalsync/core/config/WebConfig.java
+++ core/src/main/java/com/senac/dentalsync/core/config/WebConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -18,7 +17,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOriginPatterns("http://localhost:*", "https://localhost:*", "http://127.0.0.1:*") // Usar patterns em vez de origins específicas
+            .allowedOriginPatterns(
+                "http://localhost:*", 
+                "https://localhost:*", 
+                "http://127.0.0.1:*",
+                "https://dentalsyncbr.com.br",
+                "https://www.dentalsyncbr.com.br"
+            )
             .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true)
